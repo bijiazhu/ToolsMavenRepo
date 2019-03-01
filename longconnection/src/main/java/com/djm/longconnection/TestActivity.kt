fa11eb6fd3eb40c3496f57206bc9edac04d22cd9
jinmeideng <ginni.deng@yingzt.com>
2019-03-01 14:52:27 +0800

    update add maven configuration message

---
+++ longconnection/src/main/java/com/djm/longconnection/TestActivity.kt
@@ -0,0 +1,14 @@
+package com.djm.longconnection
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by ginni on 2019/3/1.
+ */
+class TestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
